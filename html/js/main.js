ce214e9b83b3b26900b34c8de4a9914a47e4ca95
Kacey A <tachk@rocketeeringetc.net>
2016-03-12 21:01:39 -0800

    Wiring up campaign entries table to read in and render data from Firebase.

--- html/js/main.js
+++ html/js/main.js
@@ -6,6 +6,8 @@ jQuery("#footer-2016").load("footer.html");
 // Main DOM events go here
 jQuery(document).on("ready", function() {
 
+    /* Login, logout, and signed-in user info */
+
     (function fixUpHeaderUI() {
         var currentAuthData = myDataRef.getAuth();
         if (currentAuthData) {
@@ -22,29 +24,83 @@ jQuery(document).on("ready", function() {
         window.location.pathname = "/";
     });
 
+    /* Campaign page functions */
+
     if (window.location.pathname === "/campaignList.html") {
-        sSelectedCampaignName = "testCampaignFake";
-        
-        $("#btnSaveCampaignName").click(function (e) {
-            sCampaignName = $("#campaignName-text").val();   // TODO: Scrub input string
-            sSelectedCampaignName = sCampaignName;
-            sReducedCampaignName = ((sCampaignName.toLowerCase()).replace(/\s/g, "")).substring(0, 10)   // For consistency with session "id"...
-            jCampaignInfo = {
-                "id": sReducedCampaignName,
-                "name": sCampaignName
-            };
-            
-            baseDataRef.child(sReducedCampaignName).update(jCampaignInfo);
+
+        // Template for a single row in the campaign list
+        function createEmptyCampaignEntryTemplate() {
+            var row = document.createElement("tr");
+            var cellMetadata = document.createElement("td");  // Campaign metadata
+            var campName = document.createElement("span");  // Holds campaign name
+            var gameSystem = document.createElement("span");  // Label for above td
+            var cellSelectBtn = document.createElement("td");  // To load campaign details
+            var campButton = document.createElement("button");  // Button for above td
+            var pageBreak = document.createElement("br");
+
+            // Setting attributes on elemenets
+            row.setAttribute("class", "plain");
+            cellMetadata.setAttribute("class", "campMetadata padded");
+            campName.setAttribute("class", "campName");
+            gameSystem.setAttribute("class", "label label-default");
+            cellSelectBtn.setAttribute("class", "campMoreInfoBtn padded");
+            campButton.setAttribute("type", "button");
+            campButton.setAttribute("class", "btn btn-s btn-primary");
+            campButton.innerText = "Choose this campaign";
+
+            // Setting up hierarchy of elements
+            cellMetadata.appendChild(campName);
+            cellMetadata.appendChild(pageBreak);
+            cellMetadata.appendChild(gameSystem);
+
+            cellSelectBtn.appendChild(campButton);
+
+            row.appendChild(cellMetadata);
+            row.appendChild(cellSelectBtn);
+
+            // In the end, just need the tr (and its children)
+            return row;
+        }
+
+        function createCampaignEntry(campaignName, rulesSystem) {
+            var campaignEntry = createEmptyCampaignEntryTemplate();
+
+            // Select first td, to retrieve campgain metadata
+            var campMetadata = campaignEntry.getElementsByTagName("td")[0];
+
+            campMetadata.querySelector("span.campName").innerText = campaignName;
+            campMetadata.querySelector("span.label-default").innerText = rulesSystem;
+
+            return campaignEntry;
+        }
+
+
+        // Loop through each campaign entry in db
+        var campaignsDataRef = new Firebase("https://shining-torch-7755.firebaseio.com/data/campaigns");
+
+        campaignsDataRef.on("value", function(snapshot) {
+
+            var dataSnapshot = snapshot.val();
+
+            for (item in dataSnapshot) {
+                var selectedCampaignName   = dataSnapshot[item].name;
+                var selectedCampaignSystem = dataSnapshot[item].system;
+                var selectedCampaignId     = dataSnapshot[item].id;
+                var newEntryForCampaign    = createCampaignEntry(selectedCampaignName, selectedCampaignSystem);
+
+                newEntryForCampaign.querySelector("button.btn-primary").setAttribute("id", selectedCampaignId);
+                document.getElementById("tblCampList").appendChild(newEntryForCampaign);
+            }
+
+        }, function(errorObject) {
+            console.log("ERROR: Failed to read DB: " + errorObject.code);
         });
-        
-        $("#btnGoHome").click(function (e) {
-            window.location = "/";
-        });
-        
-        $("#btnChooseCampaign").click(function (e) {
-            
-            window.location = "sessionList.html?campName=" + sReducedCampaignName;
-        });
+
     }
+
+    /* Navigation */
+    $("#btnGoHome").click(function (e) {
+        window.location = "/";
+    });
 
 });
