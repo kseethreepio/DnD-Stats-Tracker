1da3f0cb919bd96f2e1ba855e51a05929a715441
Kacey A <tachk@rocketeeringetc.net>
2016-07-17 04:23:41 -0700

    Creating session stats summary table on the fly by pulling player stats from DB. Removed hardcoded example entry.

--- html/js/main.js
+++ html/js/main.js
@@ -129,16 +129,16 @@ jQuery(document).on("ready", function() {
 
     }
 
-    if (window.location.pathname === "/sessionList.html" && window.location.search === "") {
+    else if (window.location.pathname === "/sessionList.html" && window.location.search === "") {
         document.getElementById("noCampaignChosen").removeAttribute("hidden");
         document.getElementById("tblSessionList").setAttribute("hidden", "true");
         document.getElementById("btnAddSession").setAttribute("style", "display: none");
     }
 
-    if (window.location.pathname === "/sessionList.html" && window.location.search !== "") {
+    else if (window.location.pathname === "/sessionList.html" && window.location.search !== "") {
 
         // Template for a single row in the campaign list
-        function createEmptySessionEntryTemplate() {
+        function createEmptySessionEntry() {
             var row              = document.createElement("tr");
             var cellSessMetadata = document.createElement("td");  // Session name
             var sessName         = document.createElement("span");
@@ -175,7 +175,7 @@ jQuery(document).on("ready", function() {
         }
 
         function createSessionEntry(sessionName, sessionId, pcNames) {
-            var sessionEntry = createEmptySessionEntryTemplate();
+            var sessionEntry = createEmptySessionEntry();
 
             // Set session name
             sessionEntry.querySelector("td.sessMetadata > span.sessName").innerText = sessionName;
@@ -247,7 +247,7 @@ jQuery(document).on("ready", function() {
 
     }
 
-    if (window.location.pathname === "/sessionStats.html" && window.location.search !== "") {
+    else if (window.location.pathname === "/sessionStats.html" && window.location.search !== "") {
 
         // TODO: Tag -- <br><span class="label label-success">Winner winner, chicken dinner</span>
         // TODO: Tag -- <br><span class="label label-danger"><a href="https://bit.ly/1JC4xw9" class="textWhite">https://bit.ly/1JC4xw9</a></span>
@@ -266,6 +266,131 @@ jQuery(document).on("ready", function() {
             }
           }
 
+        });
+
+        var sessionsDataRef = new Firebase("https://shining-torch-7755.firebaseio.com/data/sessions");
+        var statsSummaryTbl = document.querySelector("#tblSummarizeStats");
+
+        // Template for a single row in the session stats list for a single character
+        function createEmptyPlayerStatsEntry(playerName) {
+            var row                    = document.createElement("tr");
+            var cellPlayerName         = document.createElement("td");
+            var cellSkillChecks        = document.createElement("td");
+            var cellAttackRolls        = document.createElement("td");
+            var cellDeathRolls         = document.createElement("td");
+            var cellRollTotals         = document.createElement("td");
+            var spanRollSkillCrit      = document.createElement("span");
+            var spanRollSkillCritFail  = document.createElement("span");
+            var spanRollSkillTotal     = document.createElement("span");
+            var spanRollAttackCrit     = document.createElement("span");
+            var spanRollAttackCritFail = document.createElement("span");
+            var spanRollAttackTotal    = document.createElement("span");
+            var spanRollDeathCrit      = document.createElement("span");
+            var spanRollDeathCritFail  = document.createElement("span");
+            var spanRollDeathTotal     = document.createElement("span");
+            var spanTotalCrit          = document.createElement("span");
+            var spanTotalCritFail      = document.createElement("span");
+            var spanTotalRolls         = document.createElement("span");
+
+            // Setting attributes on elemenets
+            row.setAttribute("id", playerName);  // TODO: [P2] Robust handling for dupe names
+            row.setAttribute("class", "playerStats padded");
+            cellPlayerName.textContent = playerName;
+            cellPlayerName.setAttribute("class", "padded");
+            cellSkillChecks.setAttribute("class", "padded");
+            cellAttackRolls.setAttribute("class", "padded");
+            cellDeathRolls.setAttribute("class", "padded");
+            cellRollTotals.setAttribute("class", "sessionTotals padded");
+            // Setting all of the classes on the spans
+            spanRollSkillCrit.setAttribute("class", "rollSkillCrit");
+            spanRollSkillCritFail.setAttribute("class", "rollSkillCritFail");
+            spanRollSkillTotal.setAttribute("class", "rollSkillTotal");
+            spanRollAttackCrit.setAttribute("class", "rollAttackCrit");
+            spanRollAttackCritFail.setAttribute("class", "rollAttackCritFail");
+            spanRollAttackTotal.setAttribute("class", "rollAttackTotal");
+            spanRollDeathCrit.setAttribute("class", "rollDeathCrit");
+            spanRollDeathCritFail.setAttribute("class", "rollDeathCritFail");
+            spanRollDeathTotal.setAttribute("class", "rollDeathTotal");
+            spanTotalCrit.setAttribute("class", "totalCrit");
+            spanTotalCritFail.setAttribute("class", "totalCritFail");
+            spanTotalRolls.setAttribute("class", "totalRolls");
+
+            // Setting up hierarchy of elements
+            cellSkillChecks.appendChild(spanRollSkillCrit);
+            cellSkillChecks.appendChild(spanRollSkillCritFail);
+            cellSkillChecks.appendChild(spanRollSkillTotal);
+            cellAttackRolls.appendChild(spanRollAttackCrit);
+            cellAttackRolls.appendChild(spanRollAttackCritFail);
+            cellAttackRolls.appendChild(spanRollAttackTotal);
+            cellDeathRolls.appendChild(spanRollDeathCrit);
+            cellDeathRolls.appendChild(spanRollDeathCritFail);
+            cellDeathRolls.appendChild(spanRollDeathTotal);
+            cellRollTotals.appendChild(spanTotalCrit);
+            cellRollTotals.appendChild(spanTotalCritFail);
+            cellRollTotals.appendChild(spanTotalRolls);
+            row.appendChild(cellPlayerName);
+            row.appendChild(cellSkillChecks);
+            row.appendChild(cellAttackRolls);
+            row.appendChild(cellDeathRolls);
+            row.appendChild(cellRollTotals);
+
+            // In the end, just need the tr (and its children)
+            return row;
+        }
+
+        function updateStatsEntryForPlayer(playerName, statsObj) {
+            // Find the row for that player's stats
+            var playerStatsEntry = statsSummaryTbl.querySelector("#" + playerName);
+
+            // Update the skill check numbers to match what's in the DB
+            playerStatsEntry.querySelector(".rollSkillCrit").innerHTML = statsObj.skills.crits + " crits <br>";
+            playerStatsEntry.querySelector(".rollSkillCritFail").innerHTML = statsObj.skills.critFails + " crit fails <br>";
+            playerStatsEntry.querySelector(".rollSkillTotal").innerHTML = statsObj.skills.totalRolls + " total rolls <br>";
+
+            // Update the attack roll numbers to match what's in the DB
+            playerStatsEntry.querySelector(".rollAttackCrit").innerHTML = statsObj.attacks.crits + " crits <br>";
+            playerStatsEntry.querySelector(".rollAttackCritFail").innerHTML = statsObj.attacks.critFails + " crit fails <br>";
+            playerStatsEntry.querySelector(".rollAttackTotal").innerHTML = statsObj.attacks.totalRolls + " total rolls <br>";
+
+            // Update the death save throw numbers to match what's in the DB
+            playerStatsEntry.querySelector(".rollDeathCrit").innerHTML = statsObj.death.crits + " crits <br>";
+            playerStatsEntry.querySelector(".rollDeathCritFail").innerHTML = statsObj.death.critFails + " crit fails <br>";
+            playerStatsEntry.querySelector(".rollDeathTotal").innerHTML = statsObj.death.totalRolls + " total rolls <br>";
+
+            // Update the totals to match what's in the DB
+            var totCritCount = statsObj.skills.crits + statsObj.attacks.crits + statsObj.death.crits;
+            var totCritFailCount = statsObj.skills.critFails + statsObj.attacks.critFails + statsObj.death.critFails;
+            var totRollCount = statsObj.skills.totalRolls + statsObj.attacks.totalRolls + statsObj.death.totalRolls;
+            playerStatsEntry.querySelector(".totalCrit").innerHTML = totCritCount + " crits <br>";
+            playerStatsEntry.querySelector(".totalCritFail").innerHTML = totCritFailCount + " crit fails <br>";
+            playerStatsEntry.querySelector(".totalRolls").innerHTML = totRollCount + " total rolls <br>";
+
+            // Update DM mob count
+            if (playerName.indexOf("DM") != -1 || playerName.indexOf("GM") != -1) {  // Case sensitivity is intentional
+                document.querySelector("#spanNumMobs").innerText = statsObj.totalMobs;
+            }
+        }
+
+        sessionsDataRef.on("value", function(snapshot) {
+            for (item in snapshot.val()) {
+                var currSessionStats = snapshot.val()[item];
+                if (currSessionStats.id == window.location.search.split("=")[1]) {
+                    // console.log("here's the snapshot!", currSessionStats);
+                    // var sessionPlayers = currSessionStats.players.replace(/\s+/g, "").split(",");
+                    if (currSessionStats.stats !== undefined) {
+                        var sessionPlayers = Object.keys(currSessionStats.stats);
+                        for (playerNum in sessionPlayers) {
+                            var currPlayerName = sessionPlayers[playerNum];
+
+                            console.log("Player:", currPlayerName);
+                            console.log("Player's stats:", currSessionStats.stats[currPlayerName]);
+                            
+                            statsSummaryTbl.appendChild(createEmptyPlayerStatsEntry(currPlayerName));
+                            updateStatsEntryForPlayer(currPlayerName, currSessionStats.stats[currPlayerName]);
+                        }
+                    }
+                }
+            }
         });
 
     }
